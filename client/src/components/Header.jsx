4365abd3039bbc9725c4cc14defb5a1b95ecfc58
PkSmart <p1ooooo100@gmail.com>
2023-12-03 14:55:02 +0530

    created header component

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import {Link} from "react-router-dom"
+import {FaSearch} from 'react-icons/fa'
+
+function Header() {
+  return (
+    <header className="w-screen shadow-md bg-slate-200">
+      <div className="flex items-center justify-between max-w-6xl p-3 mx-auto ">
+        <Link to ="/" > 
+        <h1 className="flex flex-wrap font-bold me-2 ">
+          <span className="pt-0 pb-0 mt-0 mb-0 text-red-500 text-md/4 sm:text-xl md:text-2xl lg:text-3xl ms-0 ">Real</span>
+          <span className="pt-0 pb-0 mt-0 mb-0 text-md/4 sm:text-xl md:text-2xl lg:text-3xl ms-0 text-slate-600">Estate</span>
+        </h1>
+        </Link> 
+        <form className="flex items-center w-24 pt-1 pb-1 rounded-lg ps-2 pe-2 sm:w-64 md:p-3 bg-slate-50">
+          <input type="text" placeholder="Search..." className="bg-transparent focus:outline-none" />
+        <FaSearch className="text-slate-500 hover:cursor-pointer active:text-black" />
+        </form>
+        <ul className="flex gap-4">
+          <Link to ="/" ><li className="hidden font-semibold text-slate-700 hover:underline active:text-black sm:inline">Home</li></Link>
+          <Link to ="/about" ><li className="hidden font-semibold text-slate-700 hover:underline active:text-black sm:inline">About</li></Link>
+          <Link to ="/sign-in" ><li className="font-semibold text-slate-700 hover:underline active:text-black sm:inline">Sign in</li></Link>
+        </ul>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
