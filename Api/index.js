2f61348e8176f4d28bf3f389ac5368900616cbf7
PkSmart <p1ooooo100@gmail.com>
2023-12-11 17:33:21 +0530

    created a middleware and function that handles possible errors

--- Api/index.js
+++ Api/index.js
@@ -1,12 +1,13 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import userRouter from './routes/user.route.js'
-import authRouter from './routes/auth.route.js'
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGOURI)
+mongoose
+  .connect(process.env.MONGOURI)
   .then(() => {
     console.log("connected to db");
   })
@@ -19,7 +20,17 @@ const app = express();
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
-app.use(express.json())
+app.use(express.json());
 
-app.use('/api/user',userRouter)
-app.use('/api/auth',authRouter)
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
