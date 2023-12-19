b58b11594804fc6a8edb6ff3c64abc5870cb2b5a
PkSmart <p1ooooo100@gmail.com>
2023-12-20 01:19:23 +0530

    Added redux tool kit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,16 +1,25 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from "../redux/user/userSlice";
 
 function SignIn() {
   const [formData, setFormData] = useState({});
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
+  // const [error, setError] = useState(null);
+  // const [loading, setLoading] = useState(false);
+  const {loading, error} = useSelector((state)=> state.user)
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const handleSubmit = async (e) => {
     try {
       e.preventDefault();
-      setLoading(true);
+      // setLoading(true);
+      dispatch(signInStart())
       const res = await fetch("/api/auth/signin", {
         method: "POST",
         headers: {
@@ -22,16 +31,19 @@ function SignIn() {
       const data = await res.json();
       console.log(data);
       if (data.success === false) {
-        setError(data.message);
-        setLoading(false);
+        dispatch(signInFailure(data.message))
+        // setError(data.message);
+        // setLoading(false);
         return;
       }
-      setError(null);
-      setLoading(false);
+      dispatch(signInSuccess(data))
+      // setError(null);
+      // setLoading(false);
       navigate("/");
     } catch (error) {
-      setLoading(false);
-      setError(error.message);
+      dispatch(signInFailure(error.message))
+      // setLoading(false);
+      // setError(error.message);
       console.error("Error parsing JSON:", error);
     }
   };
