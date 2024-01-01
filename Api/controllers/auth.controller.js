a70c36504842c00765fa83c04db79c8fa213532f
PkSmart <p1ooooo100@gmail.com>
2024-01-01 23:12:56 +0530

    created update user api route

--- Api/controllers/auth.controller.js
+++ Api/controllers/auth.controller.js
@@ -5,6 +5,37 @@ import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
+
+ 
+
+  if (
+    req.body === null || // Check if req.body is null
+    typeof req.body !== "object" || // Check if req.body is an object
+    Object.keys(req.body).length === 0 || // Check if req.body is an empty object // Check if any of the required fields are missing or empty
+    req.body.username === null ||
+    req.body.username === undefined ||
+    req.body.username.trim() === "" ||
+    req.body.email === null ||
+    req.body.email === undefined ||
+    req.body.email.trim() === "" ||
+    req.body.password === null ||
+    req.body.password === undefined ||
+    req.body.password.trim() === ""
+  ) {
+    return next(
+      errorHandler(400, "Please fill in all required fields correctly.")
+    );
+  }
+
+  if (
+    /^\d+$/.test(req.body.username.trim()) || // Check if username is all numbers
+    /^\d/.test(req.body.username.trim()) // Check if username starts with a number
+    ) {
+      return next(
+        errorHandler(400, "Incorrect Username!")
+      );
+  }
+
   const hashedPassword = bcryptjs.hashSync(password, 10);
   const newUser = new User({ username, email, password: hashedPassword });
 
@@ -13,7 +44,7 @@ export const signup = async (req, res, next) => {
     res.status(201).json("user " + username + " created successfully in db");
     console.log(username, "created in db");
   } catch (error) {
-    next(error);
+    next(errorHandler(500, error));
   }
 };
 
@@ -35,7 +66,7 @@ export const signin = async (req, res, next) => {
 
     const { password: pass, ...rest } = validUser._doc;
     res
-      .cookie("access token", token, { httpOnly: true })
+      .cookie("access_token", token, { httpOnly: true })
       .status(200)
       .json(rest);
   } catch (error) {
@@ -46,16 +77,14 @@ export const signin = async (req, res, next) => {
 export const google = async (req, res, next) => {
   try {
     const user = await User.findOne({ email: req.body.email });
-    if (user)
-     {
+    if (user) {
       const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
       const { password: pass, ...rest } = user._doc;
       res
         .cookie("access token", token, { httpOnly: true })
         .status(200)
         .json(rest);
-    } else
-     {
+    } else {
       const generatedPassword =
         Math.random().toString(36).slice(-8) +
         Math.random().toString(36).slice(-8);
