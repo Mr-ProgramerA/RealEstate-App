24ddeccdc0419b92cf4395d61c39c7e0dabe5229
PkSmart <p1ooooo100@gmail.com>
2024-01-30 15:20:34 +0530

    created get user listings api route

--- Api/routes/user.route.js
+++ Api/routes/user.route.js
@@ -1,5 +1,10 @@
 import express from "express";
-import { test, updateUser, deleteUser} from "../controllers/user.controller.js";
+import {
+  test,
+  updateUser,
+  deleteUser,
+  getUserLstings,
+} from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 import { inputCheck } from "../utils/inputCheck.js";
 
@@ -8,5 +13,6 @@ const router = express.Router();
 router.get("/test", test);
 router.post("/update/:id", verifyToken, inputCheck, updateUser);
 router.delete("/delete/:id", verifyToken, deleteUser);
+router.get("/listings/:id", verifyToken, getUserLstings);
 
 export default router;
